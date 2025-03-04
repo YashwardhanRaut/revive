ab8004f8c1e0f05828e3ce49af620953f8d111fa
YashwardhanRaut <142378618+YashwardhanRaut@users.noreply.github.com>
2025-03-04 13:04:21 +0530

    before undirected

---
+++ dump/problem/read.py
@@ -0,0 +1,4 @@
+import json
+
+with open(r"C:\Users\Yashwardhan\Desktop\Revive\problem\simple_graph_bidirectional.json",'r', encoding='UTF-8') as file:
+    print(json.load(file))
--- repo/main.py
+++ repo/main.py
@@ -1,7 +1,10 @@
 import graph_display
 import graph_reader
 
-# G = graph_reader.graph_reader("un")
-G = graph_reader.graph_reader("simple_graph_bidirectionsl.json")
+G = graph_reader.graph_reader(r"C:\Users\Yashwardhan\Desktop\Revive\revive\repo\simple_graph_bidirectionsl.json")
 
 graph_display.draw_graph(G, "Initial Graph")
+
+# Add Algo here
+
+graph_display.draw_graph(G, "After Rebalancing")
